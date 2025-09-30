0b0334d8370f27d3252998a94fc19518d942b69b
koalaHaowen <z3548577@ad.unsw.edu.au>
2025-09-30 15:44:28 +1000

    Initial commit - my personal website

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/haowen_xu_phd/',   // ✅ repo name goes here
+  base: '/homepage/',   // ✅ repo name goes here
   plugins: [react()],
 })
